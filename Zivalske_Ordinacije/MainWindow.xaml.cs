3ff753b7630aa02d0aef466173f57858a103c0ea
Gapy <gasper.florjan@scv.si>
2023-02-20 22:25:52 +0100

    Dodajanje gumba zivalske ordinacije, ustavarjanje novega grida, prikaz ordinacij moznost deleta in adda

--- Zivalske_Ordinacije/MainWindow.xaml.cs
+++ Zivalske_Ordinacije/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Npgsql;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,22 +33,97 @@ namespace Zivalske_Ordinacije
             InitializeComponent();
         }
 
+        public void RefreshList()
+        {
+            ListView listView1 = new ListView();
+            listView1.Items.Clear();
+            using (var cmd = new NpgsqlCommand("SELECT * FROM IzpisOrdinacije()", con))
+            {
+                var reader = cmd.ExecuteReader();
+                DataTable dataTable = new DataTable();
+                dataTable.Load(reader);
+                GridOrdinacije.DataContext = dataTable;
+            }
+        }
+
         private void Ordinacije_Click(object sender, RoutedEventArgs e)
         {
             MainGrid.Visibility = Visibility.Hidden;
             GridOrdinacije.Visibility = Visibility.Visible;
             con.Open();
-
+            using (NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM kraji()", con))
+            {
+                using (NpgsqlDataReader read = command.ExecuteReader())
+                {
+                    while (read.Read())
+                    {
+                        string name = read.GetString(0);
+                        Kraji.Items.Add(name);
+                    }
+                }
+            }
             using (var cmd = new NpgsqlCommand("SELECT * FROM IzpisOrdinacije()", con))
             {
                 var reader = cmd.ExecuteReader();
-                GridOrdinacije.DataContext = reader;
+                DataTable dataTable = new DataTable();
+                dataTable.Load(reader);
+                GridOrdinacije.DataContext = dataTable; 
             }
-
             con.Close();
         }
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            if (ord_ime.Text == "" || ord_naslov.Text == "" || ord_mail.Text == "" || Kraji.SelectedItem == null)
+            {
+                MessageBox.Show("Can't add ordinacija without all information", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                con.Open();
 
-        
-       
+                string insert = "SELECT VnosOrdinacij('" + ord_ime.Text + "','" + ord_naslov.Text + "','" + ord_mail.Text + "','" + Kraji.SelectedItem.ToString() + "')";
+                NpgsqlCommand cmmd = new NpgsqlCommand(insert, con);
+                cmmd.ExecuteNonQuery();
+
+                RefreshList();
+
+                con.Close();
+                ord_ime.Text = "";
+                ord_naslov.Text = "";
+                ord_mail.Text = "";
+                Kraji.SelectedItem = null;
+            }
+        }
+
+        private void listView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listView.SelectedItems.Count > 0)
+            {
+                try
+                {
+                    var selectedItem = listView.SelectedItems[0] as ListViewItem;
+                    if (selectedItem != null && selectedItem.Content is DataRowView rowView)
+                    {
+                        var subItem = rowView.Row[1].ToString();
+                        ord_ime.Text = subItem;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    
+                }
+            }
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            DataRowView row = (DataRowView)listView.SelectedItem;
+            int o_id = Convert.ToInt32(row[0]);
+
+            con.Open();
+            string delete = "SELECT DeleteOrdinacije('" + o_id + "')";
+            NpgsqlCommand cmd = new NpgsqlCommand(delete, con);
+            cmd.ExecuteNonQuery();
+        }
     }
 }
