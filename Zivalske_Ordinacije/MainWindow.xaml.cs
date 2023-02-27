5eddddaf6069a7b7a87727c27674eee2f82709c1
Gapy <gasper.florjan@scv.si>
2023-02-27 18:02:53 +0100

    Koncan edit delete in vnos za veterinarja

--- Zivalske_Ordinacije/MainWindow.xaml.cs
+++ Zivalske_Ordinacije/MainWindow.xaml.cs
@@ -95,26 +95,6 @@ namespace Zivalske_Ordinacije
             }
         }
 
-        private void listView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (listView.SelectedItems.Count > 0)
-            {
-                try
-                {
-                    var selectedItem = listView.SelectedItems[0] as ListViewItem;
-                    if (selectedItem != null && selectedItem.Content is DataRowView rowView)
-                    {
-                        var subItem = rowView.Row[1].ToString();
-                        ord_ime.Text = subItem;
-                    }
-                }
-                catch (Exception ex)
-                {
-                    
-                }
-            }
-        }
-
         private void Delete_Click(object sender, RoutedEventArgs e)
         {
             DataRowView row = (DataRowView)listView.SelectedItem;
@@ -124,13 +104,11 @@ namespace Zivalske_Ordinacije
             string delete = "SELECT DeleteOrdinacije('" + o_id + "')";
             NpgsqlCommand cmd = new NpgsqlCommand(delete, con);
             cmd.ExecuteNonQuery();
-            using (var cmmd = new NpgsqlCommand("SELECT * FROM IzpisOrdinacije()", con))
-            {
-                var reader = cmmd.ExecuteReader();
-                DataTable dataTable = new DataTable();
-                dataTable.Load(reader);
-                GridOrdinacije.DataContext = dataTable;
-            }
+            RefreshList();
+            ord_ime.Text = "";
+            ord_naslov.Text = "";
+            ord_mail.Text = "";
+            Kraji.SelectedItem = null;
             con.Close();
         }
 
@@ -145,6 +123,18 @@ namespace Zivalske_Ordinacije
             MainGrid.Visibility = Visibility.Hidden;
             GridVet.Visibility = Visibility.Visible;
             con.Open();
+            using (NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM OrdIme()", con))
+            {
+                using (NpgsqlDataReader read = command.ExecuteReader())
+                {
+                    while (read.Read())
+                    {
+                        string name = read.GetString(0);
+                        Ime.Items.Add(name);
+                    }
+                }
+            }
+          
             using (var cmd = new NpgsqlCommand("SELECT * FROM IzpisVeterinarjev()", con))
             {
                 var reader = cmd.ExecuteReader();
@@ -152,6 +142,85 @@ namespace Zivalske_Ordinacije
                 dataTable.Load(reader);
                 GridVet.DataContext = dataTable;
             }
+            con.Close();
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            DataRowView row = (DataRowView)listView.SelectedItem;
+            int o_id = Convert.ToInt32(row[0]);
+
+            con.Open();
+            string edit = "SELECT EditOrdinacije('"+o_id+"','"+ord_ime.Text+"','"+ord_naslov.Text+"','"+ord_mail.Text+"','"+ Kraji.SelectedItem.ToString() + "');";
+            NpgsqlCommand cmd = new NpgsqlCommand(edit, con);
+            cmd.ExecuteNonQuery();
+            RefreshList();
+            ord_ime.Text = "";
+            ord_naslov.Text = "";
+            ord_mail.Text = "";
+            Kraji.SelectedItem = null;
+            con.Close();
+        }
+
+        private void BackVet_Click(object sender, RoutedEventArgs e)
+        {
+            GridVet.Visibility = Visibility.Hidden;
+            MainGrid.Visibility = Visibility.Visible;
+        }
+
+        private void AddVet_Click(object sender, RoutedEventArgs e)
+        {
+            if (vet_ime.Text == "" || vet_priimek.Text == "" || vet_naziv.Text == "" || Ime.SelectedItem == null)
+            {
+                MessageBox.Show("Can't add veterinar without all information", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                con.Open();
+
+                string insert = "SELECT VnosVeterinarjev('" + vet_ime.Text + "','" + vet_priimek.Text + "','" + vet_naziv.Text + "','" + Ime.SelectedItem.ToString() + "')";
+                NpgsqlCommand cmmd = new NpgsqlCommand(insert, con);
+                cmmd.ExecuteNonQuery();
+                RefreshList();
+                con.Close();
+                vet_ime.Text = "";
+                vet_priimek.Text = "";
+                vet_naziv.Text = "";
+                Ime.SelectedItem = null;
+            }
+        }
+
+        private void DeleteVet_Click(object sender, RoutedEventArgs e)
+        {
+            DataRowView row = (DataRowView)listVet.SelectedItem;
+            int v_id = Convert.ToInt32(row[0]);
+
+            con.Open();
+            string delete = "SELECT DeleteVeterinar('" + v_id + "')";
+            NpgsqlCommand cmd = new NpgsqlCommand(delete, con);
+            cmd.ExecuteNonQuery();
+            RefreshList();
+            vet_ime.Text = "";
+            vet_priimek.Text = "";
+            vet_naziv.Text = "";
+            Ime.SelectedItem = null;
+            con.Close();
+        }
+
+        private void EditVet_Click(object sender, RoutedEventArgs e)
+        {
+            DataRowView row = (DataRowView)listVet.SelectedItem;
+            int o_id = Convert.ToInt32(row[0]);
+
+            con.Open();
+            string edit = "SELECT EditVeterinar('" + o_id + "','" + vet_ime.Text + "','" + vet_priimek.Text + "','" + vet_naziv.Text + "','" + Ime.SelectedItem.ToString() + "');";
+            NpgsqlCommand cmd = new NpgsqlCommand(edit, con);
+            cmd.ExecuteNonQuery();
+            RefreshList();
+            vet_ime.Text = "";
+            vet_priimek.Text = "";
+            vet_naziv.Text = "";
+            Ime.SelectedItem = null;
             con.Close();
         }
     }
